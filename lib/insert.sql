01f50d87151ca70533cc7eedd7e32e740826e30e
Travis T <travistokuyama@gmail.com>
2018-10-30 15:17:13 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name) VALUES ("Mr. Chocolate", "Rowdy", "Tabitha", "Sergeant Brown", "Melissa", "Grinch", "Wendy", NULL)
+INSERT INTO bears (name) VALUES ("Mr. Chocolate"), ("Rowdy"), ("Tabitha"), ("Sergeant Brown"), ("Melissa"), ("Grinch"), ("Wendy"), (NULL)
